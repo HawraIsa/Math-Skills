822429bf7fc774ccdc03e8de518522cd5d72da21
Hawra Isa <halfaraj@noreply.learn.reboot01.com>
2024-02-05 10:41:58 +0000

    Update test file

--- server/test.sh
+++ server/test.sh
@@ -1,5 +1,10 @@
 # STEP 1 : give permission to the file to be executable
 chmod +x math-skills
 
-# STEP 2: run the program
+# STEP 2: run the sample program
 ./math-skills
+
+# STEP 3 : run my program
+go run main.go data.txt
+
+# STEP 4 : Compare the results

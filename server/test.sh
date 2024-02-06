c15ee32db853d4e19a879e820f05fc953c2e70c9
Hawra Isa <halfaraj@noreply.learn.reboot01.com>
2024-02-06 09:26:50 +0000

    update test file

--- server/test.sh
+++ server/test.sh
@@ -1,10 +1,14 @@
-# STEP 1 : give permission to the file to be executable
+# STEP 1: give permission to the file to be executable
 chmod +x math-skills
 
-# STEP 2: run the sample program
-./math-skills
+# STEP 2: Split the terminal into two terminals (makes the comparision easier)
+## CTRL + Shift + 5 ##
 
-# STEP 3 : run my program
+# STEP 3: run the provided program
+./math-skills
+ 
+# STEP 4: then run my program (student program)
 go run main.go data.txt
 
-# STEP 4 : Compare the results
+# STEP 5: Compare the results 
+## results should match ##
